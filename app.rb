dae5a24007624dcda93755c717fd19724c67aab3
n3m3sis42 <n3m3sis42@gmail.com>
2017-06-27 15:32:19 -0400

    done

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+  get '/say/:number/:phrase' do
+    params[:phrase] * params[:number].to_i
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @request.env["PATH_INFO"].split("/say/")[1].split("/").join(" ") + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb params[:operation].to_sym
+  end
 
 end
